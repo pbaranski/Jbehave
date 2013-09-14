b7f87d515ca35fa0977553307ebb2d7be9773251
pbaranski <pbaranski@future-processing.com>
2013-09-14 22:37:36 +0200

    Cobertura adjusted

---
+++ src/test/java/com/rhcloud/pbtest/CalculatorTest.java
@@ -0,0 +1,18 @@
+package com.rhcloud.pbtest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class CalculatorTest {
+     private Calculator calculator;
+
+    @Test
+    public void shouldAddNumbers(){
+       int number1 = 10;
+       int number2 = 5;
+        calculator.add(number1, number2);
+       assertTrue(calculator.getResult() == 15);
+    }
+
+}
